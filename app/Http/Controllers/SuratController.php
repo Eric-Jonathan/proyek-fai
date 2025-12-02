38c87fddd942e7a6bfc7e5af7112b39380919317
royce <roycevb2005@gmail.com>
2025-12-02 18:21:44 +0700

    tes

--- app/Http/Controllers/SuratController.php
+++ app/Http/Controllers/SuratController.php
@@ -8,6 +8,16 @@ use Illuminate\Support\Facades\DB;
 
 class SuratController extends Controller
 {
+    public function surat(){
+        $pdf = app(\Barryvdh\DomPDF\PDF::class);
+        $pdf->setOptions([
+            'isRemoteEnabled' => true,
+            'isHtml5ParserEnabled' => true,
+        ]);
+        $pdf->loadView('CRUD_Surat.cetak_surat');
+
+        return $pdf->download('surat_tugas.pdf');
+    }
     /**
      * Halaman utama daftar surat
      */
